d4a11c7f35f8e269dfcb1aff7b49b903abfa1d0d
maiarenan <renanmaiabh@gmail.com>
2023-05-22 12:26:35 -0300

    início

---
+++ src/componentes/OpcoesHeader/index.js
@@ -0,0 +1,9 @@
+function OpcoesHeader() {
+    return (
+        <ul className='opcoes'>
+            {textoOpcoes.map((texto) => (
+                <li className='opcao'><p>{texto}</p></li>
+            ))}
+        </ul>
+    )
+}
